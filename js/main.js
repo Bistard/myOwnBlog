50abf5fd9d1c944e209c915732de55ca2865ddeb
Bistard <1015813038@qq.com>
2020-07-29 22:10:10 +0800

    c

---
+++ js/main.js
@@ -0,0 +1,14 @@
+var swiper = new Swiper('.swiper-container', {
+    slidesPerView: 'auto',
+    centeredSlides: 3,
+    initialSlide: 2,
+    grabCursor: true,
+    slidesPerView: 5,
+
+    pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    cssMode: true,
+    cssWidthAndHeight: true
+    }
+});
